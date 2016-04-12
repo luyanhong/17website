e937854afb2b73e4010d1220c3263e3046bd05b6
Jackie <bydu007@163.com>
2016-04-12 15:54:53 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	17/static/css/index.css
    #	17/static/js/index.js

--- 17/static/js/index.js
+++ 17/static/js/index.js
@@ -4,16 +4,17 @@
 var Index = {};
 
 $(function () {
-    $("#index .border").show();
     Index.bindEvent();
 });
 
 Index.bindEvent = function(){
 
+<<<<<<< HEAD
+=======
     /*nav animate*/
     $("#nav li").hover(function(){
-        $("#nav li .border").hide();
-        $(this).find(".border").show().animate({bottom:"0px"},300);
+        $(this).find(".border").slideUp().show();
     });
 
+>>>>>>> origin/master
 };
